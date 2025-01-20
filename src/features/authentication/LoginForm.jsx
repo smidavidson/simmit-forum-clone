d1cd0018aa9d33d470cfdaffe29f6da34648c5a6
smidavidson <smidavidson@users.noreply.github.com>
2025-01-20 01:42:51 -0800

    Add CSS to login form

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -30,40 +30,64 @@ export default function LoginForm() {
     }
 
     return (
-        <form
-            onSubmit={(e) => {
-                handleSubmit(e);
-            }}
-        >
-            <div>
-                <label htmlFor='email-input'>Email:</label>
-                <input
-                    id='email-input'
-                    type='text'
-                    autoComplete='username'
-                    value={email}
-                    onChange={(e) => {
-                        setEmail((ce) => {
-                            return e.target.value;
-                        });
-                    }}
-                ></input>
-            </div>
-            <div>
-                <label htmlFor='password-input'>Password:</label>
-                <input
-                    id='password'
-                    type='password'
-                    autoComplete='current-password'
-                    value={password}
-                    onChange={(e) => {
-                        setPassword((cp) => {
-                            return e.target.value;
-                        });
-                    }}
-                ></input>
-            </div>
-            <button disabled={isLoading}>Login</button>
-        </form>
+        <div className='mx-auto mt-8 max-w-md px-4'>
+            <form
+                onSubmit={(e) => {
+                    handleSubmit(e);
+                }}
+            >
+                <h2 className='mb-6 text-2xl font-semibold'>Login to Simmit</h2>
+                <div className='space-y-6'>
+                    <div className='flex flex-col'>
+                        <label
+                            htmlFor='email-input'
+                            className='mb-1 font-medium'
+                        >
+                            Email
+                        </label>
+                        <input
+                            id='email-input'
+                            type='text'
+                            autoComplete='username'
+                            value={email}
+                            onChange={(e) => {
+                                setEmail((ce) => {
+                                    return e.target.value;
+                                });
+                            }}
+                            className='rounded-md border px-3 py-2'
+                        ></input>
+                    </div>
+                    <div className='flex flex-col'>
+                        <label
+                            htmlFor='password-input'
+                            className='mb-1 font-medium'
+                        >
+                            Password
+                        </label>
+                        <input
+                            id='password'
+                            type='password'
+                            autoComplete='current-password'
+                            value={password}
+                            onChange={(e) => {
+                                setPassword((cp) => {
+                                    return e.target.value;
+                                });
+                            }}
+                            className='rounded-md border px-3 py-2'
+                        ></input>
+                    </div>
+                </div>
+                <div className='mt-10'>
+                    <button
+                        disabled={isLoading}
+                        className='w-full rounded-md bg-gray-800 px-4 py-3 text-white'
+                    >
+                        Login
+                    </button>
+                </div>
+            </form>
+        </div>
     );
 }
