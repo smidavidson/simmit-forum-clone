9ed0818ef3072b4bad7987c7fadc320d9ce8a972
smidavidson <smidavidson@users.noreply.github.com>
2025-01-20 18:00:48 -0800

    Add styling to posts table

--- src/features/posts/PostItem.jsx
+++ src/features/posts/PostItem.jsx
@@ -1,10 +1,22 @@
 import { Link } from 'react-router-dom';
+import { formatDistance } from 'date-fns';
 
 export default function PostItem({ post }) {
+    const timeAgo = formatDistance(new Date(post.created_at), new Date(), {
+        addSuffix: true,
+    });
+
     return (
-        <div>
-            {post.title} by <Link to={`/user/${post.profiles.username}`}>{post.profiles.username}</Link> X hours ago
-            <Link to={`${post.id}`}> View Post</Link>
+        <div className='rounded-md border bg-white p-2 shadow-sm'>
+            <h2 className='text-md mb-1 font-medium'>
+                <Link to={`${post.id}`}>{post.title}</Link>
+            </h2>
+            <div className='text-sm text-gray-500'>
+                <Link to={`/user/${post.profiles.username}`} className='text-gray-700'>
+                    {post.profiles.username}
+                </Link>{' '}
+                • {timeAgo}
+            </div>
         </div>
     );
 }
--- src/features/posts/PostsTable.jsx
+++ src/features/posts/PostsTable.jsx
@@ -2,7 +2,7 @@ import PostItem from './PostItem';
 
 export default function PostsTable({ posts }) {
     return (
-        <div>
+        <div className='max-w-4xl mx-auto space-y-4 px-4'>
             {posts.map((post) => {
                 return <PostItem key={post.id} post={post}></PostItem>;
             })}
