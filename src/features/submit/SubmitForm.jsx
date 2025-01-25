7c76e4f23650b463c66836fb4f7a62cbac911cde
smidavidson <smidavidson@users.noreply.github.com>
2025-01-24 23:03:41 -0800

    Update SubmitForm to use react-hook-form, Update form entry for each submit input

--- src/features/submit/SubmitForm.jsx
+++ src/features/submit/SubmitForm.jsx
@@ -3,72 +3,98 @@ import { useNavigate } from 'react-router-dom';
 import useSubmitPost from './useSubmitPost';
 import SubmissionContentBox from '../../ui/SubmissionContentBox';
 import { BiSend } from 'react-icons/bi';
+import { useForm } from 'react-hook-form';
+import FormErrorMessage from '../../ui/FormErrorMessage';
+import Button from '../../ui/Button';
+import FormRequiredAsterix from '../../ui/FormRequiredAsterix';
+
+const labelClass = 'mb-1 font-medium inline-flex items-center';
+const inputClass = 'rounded-md border px-2 py-2';
 
 export default function SubmitForm() {
-    const [title, setTitle] = useState();
-    const [content, setContent] = useState();
+    const { register, formState, getValues, handleSubmit, reset } = useForm();
+    const { errors } = formState;
 
     const navigate = useNavigate();
 
     const { submitPost, isLoading: isSubmitting } = useSubmitPost();
 
-    function handleSubmit(e) {
-        e.preventDefault();
-
-        console.log(`Submit pressed: `, title, content);
-
+    function onSubmit({ title, content, link_url }) {
         submitPost(
-            { title, content },
+            { title, content, link_url },
             {
                 onSettled: () => {
                     // Navigate to page of newly submitted post after submission?
-                    setTitle((ct) => {
-                        return '';
-                    });
-                    setContent((cc) => {
-                        return '';
-                    });
+                    reset();
                 },
             },
         );
     }
 
     return (
-        <div>
-            <h2>Create post</h2>
-            <div>
-                <form
-                    onSubmit={(e) => {
-                        handleSubmit(e);
-                    }}
-                >
-                    <div>
-                        <label>Title{title}</label>
+        <div className='mx-auto max-w-4xl p-4'>
+            <h2 className='mb-6 text-2xl font-semibold'>Create post</h2>
+            <div className='bg-white px-6'>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className='flex flex-col'>
+                        <label className={labelClass}>
+                            Title<FormRequiredAsterix></FormRequiredAsterix>{' '}
+                            {errors?.title?.message && (
+                                <FormErrorMessage>
+                                    {errors?.title?.message}
+                                </FormErrorMessage>
+                            )}
+                        </label>
                         <input
-                            value={title}
-                            onChange={(e) => {
-                                setTitle((ct) => {
-                                    return e.target.value;
-                                });
-                            }}
+                            className={inputClass}
+                            id='title'
+                            type='text'
+                            {...register('title', {
+                                required: 'This field is required',
+                            })}
                         ></input>
                     </div>
-                    <div>
-                        <label htmlFor='post-content'>Body{content}</label>
+                    <div className='flex flex-col'>
+                        <label className={labelClass}>
+                            Link{' '}
+                            {errors?.link_url?.message && (
+                                <FormErrorMessage>
+                                    {errors?.link_url?.message}
+                                </FormErrorMessage>
+                            )}
+                        </label>
+                        <input
+                            className='rounded-md border px-2 py-2'
+                            id='link_url'
+                            {...register('link_url', {
+                                pattern: {
+                                    value: /^(https?:\/\/)?([a-zA-Z\d-]+\.)+[a-zA-Z]{2,}(\/\S*)?$/,
+                                    message: 'Please provide a valid URL',
+                                },
+                            })}
+                        ></input>
+                    </div>
+                    <div className='flex flex-col'>
+                        <label className={labelClass}>
+                            Body<FormRequiredAsterix></FormRequiredAsterix>{' '}
+                            {errors?.content?.message && (
+                                <FormErrorMessage>
+                                    {errors?.content?.message}
+                                </FormErrorMessage>
+                            )}
+                        </label>
                         <SubmissionContentBox
-                            id='post-content'
-                            value={content}
-                            onChange={(e) => {
-                                setContent((cc) => {
-                                    return e.target.value;
-                                });
-                            }}
+                            className='rounded-md'
+                            id='content'
+                            {...register('content', {
+                                required: 'This field is required',
+                            })}
                             placeholder='Say something about your post!'
                         ></SubmissionContentBox>
                     </div>
-                    <button>
+                    <Button variant='small' disabled={isSubmitting}>
                         Post<BiSend></BiSend>
-                    </button>
+                    </Button>
                 </form>
             </div>
         </div>
