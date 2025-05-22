37c16950ea831771fbbff1579684bd7ed1e9c442
smidavidson <87210759+smidavidson@users.noreply.github.com>
2025-05-22 02:10:35 -0700

    Update apiFlairs.js to support sorting

--- src/services/apiFlairs.js
+++ src/services/apiFlairs.js
@@ -1,16 +1,23 @@
-import toast from "react-hot-toast";
-import supabase from "./supabase";
+import toast from 'react-hot-toast';
+import supabase from './supabase';
 
 // Get a specific single Post record given a Post ID
 export async function getFlairs() {
-    const { data: flairs, error } = await supabase
-        .from('flairs')
-        .select(`*`)
+    try {
+        const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/flairs`, {
+            credentials: 'include',
+        });
 
-    if (error) {
+        const resData = await res.json();
+
+        if (!res.ok) {
+            toast.error(resData.message || 'Failed to fetch flairs');
+            throw new Error(resData.message || 'Failed to fetch flairs');
+        }
+
+        return resData;
+    } catch (error) {
         toast.error(error.message);
-        throw new Error(error);
+        throw error;
     }
-
-    return flairs;
 }
